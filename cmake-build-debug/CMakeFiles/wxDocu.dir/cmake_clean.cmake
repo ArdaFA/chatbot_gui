c4932d277e42888d4a86bae7fa59c923fc3e17e3
ardafikretazakli <ardafikretazakli@gmail.com>
2025-05-21 14:09:08 +0200

    :d

---
+++ cmake-build-debug/CMakeFiles/wxDocu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wxDocu.dir/main.cpp.o"
+  "CMakeFiles/wxDocu.dir/main.cpp.o.d"
+  "wxDocu"
+  "wxDocu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wxDocu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
